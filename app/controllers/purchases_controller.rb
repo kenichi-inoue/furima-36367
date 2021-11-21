81e32b4a6e160a0f5f3630ae0251bc9bde1d9ceb
kenichi-inoue <ken1.inoue13@gmail.com>
2021-11-21 21:32:35 +0900

    チェック

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,12 +5,10 @@ class PurchasesController < ApplicationController
   def index
     # フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
     @purchase_ship = PurchaseShip.new
-    # @product = Product.find(params[:product_id])
   end
 
   def create
     @purchase_ship = PurchaseShip.new(purchase_params)
-    # @product = Product.find(params[:product_id])
     if @purchase_ship.valid?
       pay_item
       @purchase_ship.save
@@ -32,7 +30,6 @@ class PurchasesController < ApplicationController
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
 
     Payjp::Charge.create(
-      # amount: purchase_params[:purchase_price],  # 商品の値段
       amount: @product.purchase_price,
       card: purchase_params[:token], # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
