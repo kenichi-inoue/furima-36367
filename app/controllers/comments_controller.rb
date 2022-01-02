b331efc89cf0d73bb8f63dccf26f53da48973dcd
kenichi-inoue <ken1.inoue13@gmail.com>
2022-01-02 15:43:15 +0900

    コントローラーからJavascriptに情報を送信
    チャネルを作成
    comment_channel.rbを編集し、チャネルを設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,13 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to product_path(params[:product_id])
+      ActionCable.server.broadcast "comment_channel", {comment: @comment, user: @comment.user} #追加
+      # redirect_to product_path(params[:product_id]) #削除
     end
   end
 
   private
-  
+
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, product_id: params[:product_id])
   end
